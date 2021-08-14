89d3b5d71e49a2941ea8f807cf89bdb0e7e4ecd2
vavesparashar <vavesparashar@gmail.com>
2021-08-14 11:15:57 +0530

    Update newsletter.js

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -117,13 +117,11 @@ const NewsletterPage = ({location}) => {
                 </ul>
               </div>
             </div>
-            .center {
-                text-align: center;
-                <p>
-                <script async src="https://eomail5.com/form/07c39577-fcb5-11eb-96e5-06b4694bee2a.js" data-form="07c39577-fcb5-11eb-96e5-06b4694bee2a"></script>
-                </p>
+<P>             <script async src="https://eomail5.com/form/07c39577-fcb5-11eb-96e5-06b4694bee2a.js" data-form="07c39577-fcb5-11eb-96e5-06b4694bee2a"></script>
+</P>
+                
               
-                  }
+                 
             
             <p className="newsletter-note">
               After signing up, you'll get an email to confirm your subscription.
